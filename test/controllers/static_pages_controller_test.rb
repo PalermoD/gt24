307051a9edc852ccdb5641eafd15d91e1960409c
PalermoD <palermo.deschamps@gmail.com>
2015-06-14 13:59:00 -0400

    added tests for titles on static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "GT24 | New York's very own social network and media experience"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | New York's very own social network and media experience"
+  end
+
+  test "should get about" do 
+  	get :about
+  	assert_response :success
+  	assert_select "title", "About | New York's very own social network and media experience"
   end
 
 end
