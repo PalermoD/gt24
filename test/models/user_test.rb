8dab99597ce6ce6e3fe422e8c35bcce55c8471eb
PalermoD <palermo.deschamps@gmail.com>
2015-06-17 14:02:12 -0400

    added some validation tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+	def setup 
+		@user = User.new(name: "Palermo Deschamps", email: "palermo@email.com")
+	end 
+
+	test "should be valid" do 
+		assert @user.valid?
+	end 
+
+	test "name should be present" do 
+		@user.name  = "      "
+		assert_not @user.valid?
+	end 
+
+	test "email should be present" do 
+		@user.email = "      "
+		assert_not @user.valid?
+	end 
+
+	test "name should not be too long" do 
+		@user.name = "a" * 51 
+		assert_not @user.valid?
+	end 
+
+	test "email should not be too long" do 
+		@user.email = "a" * 250 + "@email.com"
+		assert_not @user.valid?
+	end 
+
+	test "email validations should accept valid adresses" do 
+		invalid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+        invalid_addresses.each do |invalid_address|
+        	@user.email = invalid_address
+            assert @user.valid?, "#{invalid_address.inspect} should be valid"
+        end 
+    end 
+end 
