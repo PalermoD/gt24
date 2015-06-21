9b8b01721685d672640a9b40214eb2a2bbdd2190
PalermoD <palermo.deschamps@gmail.com>
2015-06-21 12:00:42 -0400

    created if stament for home page if user is logged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,20 @@ class PostsController < ApplicationController
 	before_action :loged_in_user, only: [:create, :destroy]
 
 	def create
+		@post = current_user.microposts.build(post_params)
+        if @post.save
+          redirect_to root_url
+        else
+          render 'static_pages/home'
+        end
 	end 
 
 	def destroy
 	end 
+
+	private 
+
+	 def post_params
+      params.require(:post).permit(:content)
+    end
 end
